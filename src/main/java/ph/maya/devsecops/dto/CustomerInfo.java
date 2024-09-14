f7888b0ca15ca652a8858decd9c22cbf01382f62
Eric Tiongson <eric@tiongson.ph>
2024-09-14 18:47:02 +0800

    Add CustomerInfo DTO class

---
+++ src/main/java/ph/maya/devsecops/dto/CustomerInfo.java
@@ -0,0 +1,11 @@
+package ph.maya.devsecops.dto;
+
+public class CustomerInfo {
+  private String customerId;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phoneNumber;
+  private String address;
+  private Integer zipCode;
+}
