088b9720f4cfd72655df5bcff3957404251c3eb9
Eric Tiongson <eric@tiongson.ph>
2024-09-14 18:29:38 +0800

    initial commit

---
+++ src/main/java/ph/maya/devsecops/maven_test/MavenTestApplication.java
@@ -0,0 +1,13 @@
+package ph.maya.devsecops.maven_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/maya/devsecops/maven_test/MavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.maya.devsecops.maven_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
